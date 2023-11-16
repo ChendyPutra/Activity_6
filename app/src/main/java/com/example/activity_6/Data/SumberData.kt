bea8c30e37ee7f89fb1e12f672ccabfaaf790db3
Chendy Lundy <chendy.lundy.ft21@mail.umy.ac.id>
2023-11-16 12:32:28 +0700

    Menambahkan Class object SumberData

---
+++ app/src/main/java/com/example/activity_6/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.activity_6.Data
+
+import com.example.activity_6.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.cokelat,
+        R.string.lyche
+    )
+}
